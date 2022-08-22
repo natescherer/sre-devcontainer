814e1d8b1f6dc86c70106385651f82e4cc99d38e
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-08-22 13:00:40 -0400

    Improve pwsh module and py package management

--- src/.devcontainer/library/versioninfo.py
+++ src/.devcontainer/library/versioninfo.py
@@ -1,66 +1,66 @@
 import subprocess
 import json
 
-print("AWS")
+# AWS
 aws_raw = subprocess.run(
     ["aws", "--version"],
     check=True,
     stdout=subprocess.PIPE
 ).stdout.decode("utf-8")
 aws_ver = aws_raw.splitlines()[0].split(" ")[0].split("/")[1]
-print(f"| AWS CLI | {aws_ver} |")
 
-print("Azure")
+
+# Azure
 az_raw = subprocess.run(
     ["az", "--version", "--only-show-errors"],
     check=True,
     stdout=subprocess.PIPE
 ).stdout.decode("utf-8")
 az_ver = az_raw.splitlines()[0].split(" ")[-1]
-print(f"| Azure CLI | {az_ver} |")
 
-print("Git")
+
+# Git
 git_raw = subprocess.run(
     ["git", "--version"],
     check=True,
     stdout=subprocess.PIPE
 ).stdout.decode("utf-8")
 git_ver = git_raw.splitlines()[0].split(" ")[-1]
-print(f"| Git | {git_ver} |")
 
-print("GitHub")
+
+# GitHub
 gh_raw = subprocess.run(
     ["gh", "--version"],
     check=True,
     stdout=subprocess.PIPE
 ).stdout.decode("utf-8")
 gh_ver = gh_raw.splitlines()[0].split(" (")[0].split(" ")[-1]
-print(f"| GitHub CLI | {gh_ver} |")
 
-print("Jupyter")
+
+# Jupyter
 jup_raw = subprocess.run(
     ["jupyter", "lab", "--version"],
     check=True,
     stdout=subprocess.PIPE
 ).stdout.decode("utf-8")
 jup_ver = jup_raw.splitlines()[0]
-print(f"| Jupyter Lab | {jup_ver} |")
 
-print("Kubernetes")
+
+# Kubernetes
 helm_raw = subprocess.run(
     ["helm", "version"],
     check=True,
     stdout=subprocess.PIPE
 ).stdout.decode("utf-8")
 helm_ver = helm_raw.splitlines()[0].split("Version:\"v")[1].split("\"")[0]
-print(f"| Helm | {helm_ver} |")
+
 k9s_raw = subprocess.run(
     ["k9s", "version", "-s"],
     check=True,
     stdout=subprocess.PIPE
 ).stdout.decode("utf-8")
 k9s_ver = k9s_raw.splitlines()[0].split(" ")[-1]
-print(f"| k9s | {k9s_ver} |")
+
 kubectl_raw = subprocess.run(
     ["kubectl", "version", "--short"],
     check=False,
@@ -68,55 +68,62 @@ kubectl_raw = subprocess.run(
     stderr=subprocess.DEVNULL
 ).stdout.decode("utf-8")
 kubectl_ver = kubectl_raw.splitlines()[0].split(" v")[1]
-print(f"| kubectl | {kubectl_ver} |")
+
 kubectx_raw = subprocess.run(
     ["brew", "list", "--versions", "kubectx"],
     check=True,
     stdout=subprocess.PIPE
 ).stdout.decode("utf-8")
 kubectx_ver = kubectx_raw.splitlines()[0].split(" ")[1]
-print(f"| kubectx | {kubectx_ver} |")
-print(f"| kubens | {kubectx_ver} |")
 
-print("PowerShell")
+
+# PowerShell
 pwsh_raw = subprocess.run(
     ["pwsh", "--version"],
     check=True,
     stdout=subprocess.PIPE
 ).stdout.decode("utf-8")
 pwsh_ver = pwsh_raw.splitlines()[0].split(" ")[1]
-print(f"| PowerShell | {pwsh_ver} |")
-mod_json = subprocess.run(
+
+
+# PowerShell Modules
+pwsh_modules_json = subprocess.run(
     ["pwsh", "-c", "Get-InstalledModule | "
-     "Select-Object Name,Version | "
-     "Sort-Object Name | ConvertTo-Json"
+     "Select-Object @{Name='Tool';Expression={$_.Name}},Version | "
+     "Sort-Object Tool | ConvertTo-Json"
      ],
     check=True,
     stdout=subprocess.PIPE
 ).stdout.decode("utf-8")
-mod_array = json.loads(mod_json)
-for mod in mod_array:
-    print(f"| PowerShell Module: {mod['Name']} | {mod['Version']} |")
+pwsh_modules = json.loads(pwsh_modules_json)
 
-print("Python")
+
+# Python
 py_raw = subprocess.run(
     ["python", "--version"],
     check=True,
     stdout=subprocess.PIPE
 ).stdout.decode("utf-8")
 py_ver = py_raw.splitlines()[0].split(" ")[1]
-print(f"| Python | {py_ver} |")
-pkg_json = subprocess.run(
+
+
+# Python Packages
+pip_json = subprocess.run(
     ["pip3", "list", "--format", "json"],
     check=False,
     stdout=subprocess.PIPE,
     stderr=subprocess.DEVNULL
 ).stdout.decode("utf-8")
-pkg_array = json.loads(pkg_json)
-for pkg in pkg_array:
-    print(f"| Python Package: {pkg['name']} | {pkg['version']} |")
+pip_array = json.loads(pip_json)
+py_packages = []
+for pkg in pip_array:
+    py_packages += {
+        "Tool": pkg["name"],
+        "Version": pkg["version"]
+    }
 
-print("SSH")
+
+# SSH
 ssh_raw = subprocess.run(
     ["ssh", "-V"],
     check=True,
@@ -124,17 +131,23 @@ ssh_raw = subprocess.run(
     stderr=subprocess.PIPE
 ).stderr.decode("utf-8")
 ssh_ver = ssh_raw.splitlines()[0].split("_")[1].split(" ")[0]
-print(f"| OpenSSH Client | {ssh_ver} |")
-print(f"| OpenSSH Server | {ssh_ver} |")
 
-print("Terraform")
+
+# Terraform
 tf_raw = subprocess.run(
     ["terraform", "-version"],
     check=True,
     stdout=subprocess.PIPE
 ).stdout.decode("utf-8")
 tf_ver = tf_raw.splitlines()[0].split(" v")[1]
-print(f"| Terraform CLI | {tf_ver} |")
+
+terragrunt_raw = subprocess.run(
+    ["terragrunt", "-v"],
+    check=True,
+    stdout=subprocess.PIPE
+).stdout.decode("utf-8")
+terragrunt_ver = terragrunt_raw.splitlines()[0].split(" v")[-1]
+
 tflint_raw = subprocess.run(
     ["tflint", "-v"],
     check=True,
@@ -142,10 +155,169 @@ tflint_raw = subprocess.run(
 ).stdout.decode("utf-8")
 tflint_ver = tflint_raw.splitlines()[0].split(" ")[-1]
 print(f"| TFLint | {tflint_ver} |")
-terragrunt_raw = subprocess.run(
-    ["terragrunt", "-v"],
-    check=True,
-    stdout=subprocess.PIPE
-).stdout.decode("utf-8")
-terragrunt_ver = terragrunt_raw.splitlines()[0].split(" v")[-1]
-print(f"| Terragrunt | {terragrunt_ver} |")
+
+output_data = [
+    {
+        "Technology": "AWS",
+        "Tools": [
+            {
+                "Tool": "AWS CLI",
+                "Version": aws_ver
+            },
+            {
+                "Tool": "AWSPowerShell.NetCore PowerShell Module",
+                "Version": [x for x in pwsh_modules
+                            if x["Tool"] == "AWSPowerShell.NetCore"][0]["Version"]
+            },
+            {
+                "Tool": "boto3 Python Package",
+                "Version": [x for x in py_packages
+                            if x["Tool"] == "boto3"][0]["Version"]
+            },
+            {
+                "Tool": "s3transfer Python Package",
+                "Version": [x for x in py_packages
+                            if x["Tool"] == "s3transfer"][0]["Version"]
+            }
+        ]
+    },
+    {
+        "Technology": "Azure",
+        "Tools": [
+            {
+                "Tool": "Az PowerShell Module",
+                "Version": [x for x in pwsh_modules
+                            if x["Tool"] == "Az"][0]["Version"]
+            },
+            {
+                "Tool": "Azure CLI",
+                "Version": az_ver
+            },
+            {
+                "Tool": "CosmosDB PowerShell Module",
+                "Version": [x for x in pwsh_modules
+                            if x["Tool"] == "CosmosDB"][0]["Version"]
+            },
+            {
+                "Tool": "Microsoft.Graph PowerShell Module",
+                "Version": [x for x in pwsh_modules
+                            if x["Tool"] == "Microsoft.Graph"][0]["Version"]
+            },
+            {
+                "Tool": "SqlServer PowerShell Module",
+                "Version": [x for x in pwsh_modules
+                            if x["Tool"] == "SqlServer"][0]["Version"]
+            }
+        ]
+    },
+    {
+        "Technology": "Git",
+        "Tools": [
+            {
+                "Tool": "Git",
+                "Version": git_ver
+            }
+        ]
+    },
+    {
+        "Technology": "GitHub",
+        "Tools": [
+            {
+                "Tool": "GitHub CLI",
+                "Version": gh_ver
+            }
+        ]
+    },
+    {
+        "Technology": "Jupyter",
+        "Tools": [
+            {
+                "Tool": "Jupyter Lab",
+                "Version": jup_ver
+            }
+        ]
+    },
+    {
+        "Technology": "Kubernetes",
+        "Tools": [
+            {
+                "Tool": "Helm",
+                "Version": helm_ver
+            },
+            {
+                "Tool": "k9s",
+                "Version": k9s_ver
+            },
+            {
+                "Tool": "kubectl",
+                "Version": kubectl_ver
+            },
+            {
+                "Tool": "kubectx",
+                "Version": kubectx_ver
+            },
+            {
+                "Tool": "kubens",
+                "Version": kubectx_ver
+            }
+        ]
+    },
+    {
+        "Technology": "PowerShell",
+        "Tools": [
+            {
+                "Tool": "PowerShell",
+                "Version": pwsh_ver
+            }
+        ]
+    },
+    {
+        "Technology": "PowerShell Modules",
+        "Tools": pwsh_modules
+    },
+    {
+        "Technology": "Python",
+        "Tools": [
+            {
+                "Tool": "Python",
+                "Version": py_ver
+            }
+        ]
+    },
+    {
+        "Technology": "Python Packages",
+        "Tools": py_packages
+    },
+    {
+        "Technology": "SSH",
+        "Tools": [
+            {
+                "Tool": "OpenSSH Client",
+                "Version": ssh_ver
+            },
+            {
+                "Tool": "OpenSSH Server",
+                "Version": ssh_ver
+            }
+        ]
+    },
+    {
+        "Technology": "Terraform",
+        "Tools": [
+            {
+                "Tool": "Terraform CLI",
+                "Version": tf_ver
+            },
+            {
+                "Tool": "Terragrunt",
+                "Version": terragrunt_ver
+            },
+            {
+                "Tool": "TFLint",
+                "Version": tflint_ver
+            }
+        ]
+    }
+]
+
+print(json.dumps(output_data))

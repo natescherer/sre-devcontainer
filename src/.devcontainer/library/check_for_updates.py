293b01065298f34046bd2777459d287929bb2c3d
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-08-22 17:43:42 -0400

    Improve update notifications

--- src/.devcontainer/library/check_for_updates.py
+++ src/.devcontainer/library/check_for_updates.py
@@ -2,6 +2,7 @@
  released on GitHub"""
 import semver
 import requests
+from rich import print # noqa
 
 with open("/opt/sredevcontainer/VERSION", encoding="utf-8") as f:
     current_version = f.readline(0)
@@ -10,5 +11,6 @@ latest_version = requests.get(
     "https://raw.githubusercontent.com/natescherer/sre-devcontainer/main/LATESTVERSION") # noqa
 
 if (semver.compare(current_version, latest_version)) == -1:
-    print("Current version of SRE devcontainer is out of date! Please run "
-        "'Rebuild and Reopen in Container' task in VSCode to update.")
+    print("[bold red]Current version of SRE devcontainer is out of date!"
+        " Please run 'Rebuild and Reopen in Container' task in VSCode to"
+        " update.[/bold red]")

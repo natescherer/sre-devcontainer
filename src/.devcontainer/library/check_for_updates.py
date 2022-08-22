3912445e2caea11f776cd1f2eda38ea7a981e0ce
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-08-22 18:02:30 -0400

    Improve visibility

--- src/.devcontainer/library/check_for_updates.py
+++ src/.devcontainer/library/check_for_updates.py
@@ -5,12 +5,12 @@ import requests
 from rich import print # noqa
 
 with open("/opt/sredevcontainer/VERSION", encoding="utf-8") as f:
-    current_version = f.readline(0)
+    current_version = f.readlines()[0]
 
 latest_version = requests.get(
-    "https://raw.githubusercontent.com/natescherer/sre-devcontainer/main/LATESTVERSION") # noqa
+    "https://raw.githubusercontent.com/natescherer/sre-devcontainer/main/LATESTVERSION").text # noqa
 
 if (semver.compare(current_version, latest_version)) == -1:
-    print("[bold red]Current version of SRE devcontainer is out of date!"
+    print("[reverse red]Current version of SRE devcontainer is out of date!"
         " Please run 'Rebuild and Reopen in Container' task in VSCode to"
-        " update.[/bold red]")
+        " update.[/reverse red]")

7fcec2c511fd692697b2eeb5aa50bf9d4a8aa525
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-08-23 17:30:21 -0400

    Add general build bugfixes

--- src/.devcontainer/library/install_sre_tools.sh
+++ src/.devcontainer/library/install_sre_tools.sh
@@ -4,7 +4,7 @@ bash /opt/sredevcontainer/powershell-debian.sh
 rm -rf /var/lib/apt/lists/*
 
 # Install manual software
-. manual_install_functions.sh
+source manual_install_functions.sh
 install_from_github ahmetb kubectx kubectx_vVERSION_linux_ARCH.tar.gz /opt/kubectx/kubectx
 install_from_github ahmetb kubectx kubens_vVERSION_linux_ARCH.tar.gz /opt/kubens/kubens
 install_from_github derailed k9s k9s_Linux_ARCH.tar.gz /opt/k9s/k9s
@@ -12,7 +12,7 @@ install_from_github derailed k9s k9s_Linux_ARCH.tar.gz /opt/k9s/k9s
 # Set up zsh and pwsh profiles
 UPDATE_SCRIPT="python3 /opt/sredevcontainer/check_for_updates.py"
 echo $UPDATE_SCRIPT >> /home/vscode/.zshrc
-mkdir -p /home/vscode/.config/powershell
+sudo -u vscode mkdir -p /home/vscode/.config/powershell
 echo $UPDATE_SCRIPT > /home/vscode/.config/powershell/profile.ps1
 
 # Install Python packages

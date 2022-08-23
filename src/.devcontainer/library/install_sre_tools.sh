3bc5b2ebe79e86723ded7b9019c6faaa6b322cbe
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-08-23 18:08:01 -0400

    Disable version check temporarily

--- src/.devcontainer/library/install_sre_tools.sh
+++ src/.devcontainer/library/install_sre_tools.sh
@@ -6,7 +6,7 @@ bash /opt/sredevcontainer/powershell-debian.sh
 rm -rf /var/lib/apt/lists/*
 
 # Install manual software
-source manual_install_functions.sh
+source /opt/sredevcontainer/manual_install_functions.sh
 install_from_github ahmetb kubectx kubectx_vVERSION_linux_ARCH.tar.gz /opt/kubectx/kubectx
 install_from_github ahmetb kubectx kubens_vVERSION_linux_ARCH.tar.gz /opt/kubens/kubens
 install_from_github derailed k9s k9s_Linux_ARCH.tar.gz /opt/k9s/k9s

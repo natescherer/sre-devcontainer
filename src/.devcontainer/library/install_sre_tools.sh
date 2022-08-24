ec4a888c6d6ccdaac160085228c12804b96563c7
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-08-24 13:35:04 -0400

    Fix spacing [skip ci]

--- src/.devcontainer/library/install_sre_tools.sh
+++ src/.devcontainer/library/install_sre_tools.sh
@@ -16,6 +16,7 @@ sudo -u vscode mkdir -p /home/vscode/.config/powershell
 UPDATE_SCRIPT=""
 sudo -u vscode echo "python3 /opt/sredevcontainer/check_for_updates.py > 2>&1" >> /home/vscode/.zshrc
 sudo -u vscode echo "Start-Process -FilePath 'python3' -ArgumentList '/opt/sredevcontainer/check_for_updates.py' -Wait | Write-Warning" > /home/vscode/.config/powershell/profile.ps1
+
 # Install Python packages
 pip3 --no-cache-dir install --upgrade pip
 pip3 --no-cache-dir install -r /opt/sredevcontainer/requirements.txt

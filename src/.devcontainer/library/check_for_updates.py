3b95a9ddba0606d023528120213b7ed6fc96dc5f
Nate Scherer <376408+natescherer@users.noreply.github.com>
2023-06-13 15:29:00 -0400

    Update version checking to use Github Releases

--- src/.devcontainer/library/check_for_updates.py
+++ src/.devcontainer/library/check_for_updates.py
@@ -1,14 +1,25 @@
 """Checks to see if there is a newer version of the SRE devcontainer
  released on GitHub"""
-import semver
+import sys
 import requests
-from rich import print # noqa
+import semver
+from rich import print # pylint: disable=redefined-builtin
 
-with open("/opt/sredevcontainer/VERSION", encoding="utf-8") as f:
-    current_version = f.readlines()[0]
+# with open("/opt/sredevcontainer/VERSION", encoding="utf-8") as f:
+#     current_version = f.readlines()[0]
 
-latest_version = requests.get(
-    "https://raw.githubusercontent.com/natescherer/sre-devcontainer/main/LATESTVERSION").text # noqa
+current_version = "0.0.0"
+
+api_uri = "https://api.github.com/repos/natescherer/sre-devcontainer/releases/latest"
+try:
+    api_response = requests.get(api_uri, timeout=10).json()
+except requests.exceptions.ConnectionError:
+    print("[reverse red]Error connecting to GitHub to check for SRE "
+          "devcontainer updates. Make sure your internet connection is "
+          "working.[/reverse red]")
+    sys.exit(1)
+
+latest_version = api_response["tag_name"].replace("v","")
 
 if (semver.compare(current_version, latest_version)) == -1:
     print("[reverse red]Current version of SRE devcontainer is out of date!"
